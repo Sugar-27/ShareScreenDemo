8b794eb937479ba58f8ae9b6382ea29ade4c3041
Sugar <45682h@gmail.com>
2022-11-13 14:39:50 +0800

    搭建推拉流框架

--- static/js/screen_share.js
+++ static/js/screen_share.js
@@ -1,6 +1,7 @@
 'use strict'
 
 const localVideo = document.getElementById('localVideo')
+const remoteVideo = document.getElementById('remoteVideo')
 
 const startPushBtn = document.getElementById('btnStartPush')
 const endPushBtn = document.getElementById('btnEndPush')
@@ -9,14 +10,54 @@ const endPullBtn = document.getElementById('btnEndPull')
 
 startPushBtn.addEventListener('click', startPush)
 
+startPullBtn.addEventListener('click', startPull)
+
+const config = {}
+const offerOptions = {
+    offerToReciveAudio: false,
+    offerToReciveVideo: false
+}
+
+let pc1 = new RTCPeerConnection(config) // local pc
+let pc2 = new RTCPeerConnection(config) // remote pc
+
 function startPush() {
     console.log("start push stream...")
 
     window.postMessage({type: 'SS_UI_REQUEST', text: "push"}, '*')
 }
 
+function startPull() {
+    console.log("start pull stream...")
+
+    pc2.createAnswer().then(
+        onCreateAnserSuccess,
+        onCreateSessionDescriptionError
+    )
+}
+
+function onCreateAnserSuccess(desc) {
+    console.log('anser from pc2: \n' + desc.sdp)
+
+    console.log('pc2 set local description start')
+    pc2.setLocalDescription(desc).then(
+        function() {
+            onSetLocalSuccess(pc2)
+        },
+        onSetSessionDescriptionError
+    )
+
+    // 交换sdp
+    pc1.setRemoteDescription(desc).then(
+        function() {
+            onSetRemoteSuccess(pc1)
+        },
+        onSetSessionDescriptionError
+    )
+}
+
 window.addEventListener('message', function(event) {
-    if (event.origin != this.window.location.origin) {
+    if (event.origin != window.location.origin) {
         return
     }
 
@@ -47,6 +88,95 @@ function startScreenStreamFrom(streamId) {
 function handleSuccess(stream) {
     console.log("get screen stream success")
     localVideo.srcObject = stream
+
+    pc1.oniceconnectionstatechange = function(e) {
+        onIcegatheringstatechange(pc1, e)
+    }
+
+    pc1.onicecandidate = function(e) {
+        onIceCandidate(pc1, e)
+    }
+
+    pc1.addStream(stream)
+
+    pc1.createOffer(offerOptions).then(
+        onCreateOfferSuccess,
+        onCreateSessionDescriptionError
+    )
+}
+
+function getPc(pc) {
+    return pc == pc1 ? 'pc1' : 'pc2'
+}
+
+function onCreateOfferSuccess(desc) {
+    console.log('offer from pc1: \n' + desc.sdp)
+
+    console.log('pc1 set local description start')
+    pc1.setLocalDescription(desc).then(
+        function() {
+            onSetLocalSuccess(pc1)
+        },
+        onSetSessionDescriptionError()
+    )
+
+    // sdp交换
+    pc2.oniceconnectionstatechange = function(e) {
+        onIcegatheringstatechange(pc2, e)
+    }
+
+    pc2.onicecandidate = function(e) {
+        onIceCandidate(pc2, e)
+    }
+
+    pc2.onaddstream = function(e) {
+        console.log('pc2 receive stream, stream_id: ' + e.stream.id)
+        remoteVideo.srcObject = e.stream
+    }
+
+    pc2.setRemoteDescription(desc).then(
+        function() {
+            onSetRemoteSuccess(pc2)
+        },
+        onSetSessionDescriptionError
+    )
+}
+
+function onSetLocalSuccess(pc) {
+    console.log(getPc(pc) + ' set local success')
+}
+
+function onSetRemoteSuccess(pc) {
+    console.log(getPc(pc) + ' set remote success')
+}
+
+function onCreateSessionDescriptionError(err) {
+    console.log('create session description error ' + err.toString())
+}
+
+function onSetSessionDescriptionError(err) {
+    console.log('set session description error ' + err.toString())
+}
+
+function onIcegatheringstatechange(pc, e) {
+    console.log(getPc(pc) + ' ice state change: ' + pc.iceConnectionState)
+}
+
+function onIceCandidate(pc, e) {
+    console.log(getPc(pc) + ' get new ice candidate: ' + (e.candidate ? e.candidate.candidate : '(null)'))
+
+    getOther(pc).addIceCandidate(e.candidate).then(
+        function() {
+            console.log(getPc(getOther(pc)) + ' add ice candidate success')
+        },
+        function(err) {
+            console.log(getPc(getOther(pc)) + ' add ice candidate error: ' + err.toString())
+        }
+    )
+}
+
+function getOther(pc) {
+    return pc == pc1 ? pc2 : pc1
 }
 
 function handleError(err) {
